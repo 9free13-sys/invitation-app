33a27af61492bbfeb8d1b190918f36925437ea44
Teu Nome <teuemail@gmail.com>
2026-03-29 23:33:13 +0100

    corrige link de convite no admin

--- guests/admin.py
+++ guests/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from .models import Guest
 from django.utils.html import format_html
+from .models import Guest
+
 
 @admin.register(Guest)
 class GuestAdmin(admin.ModelAdmin):
@@ -9,6 +10,6 @@ class GuestAdmin(admin.ModelAdmin):
 
     def invite_link(self, obj):
         url = f"https://invitation-app-auto.onrender.com/invite/{obj.token}/confirm/"
-        return format_html(f'<a href="{url}" target="_blank">Link</a>')
+        return format_html('<a href="{}" target="_blank">Link</a>', url)
 
     invite_link.short_description = "Convite"
