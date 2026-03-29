66d92ddaf2cad6773b335458f201a745a757f280
Teu Nome <teuemail@gmail.com>
2026-03-29 23:29:22 +0100

    adiciona link de convite no admin

--- guests/admin.py
+++ guests/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 from .models import Guest
+from django.utils.html import format_html
 
 @admin.register(Guest)
 class GuestAdmin(admin.ModelAdmin):
-    list_display = ('full_name', 'event', 'phone', 'status')
-    readonly_fields = ('token',) 
-    list_filter = ('event', 'status')
-    search_fields = ('full_name', 'phone', 'email')
+    list_display = ('full_name', 'event', 'phone', 'status', 'invite_link')
+    readonly_fields = ('token',)
+
+    def invite_link(self, obj):
+        url = f"https://invitation-app-auto.onrender.com/invite/{obj.token}/confirm/"
+        return format_html(f'<a href="{url}" target="_blank">Link</a>')
+
+    invite_link.short_description = "Convite"
