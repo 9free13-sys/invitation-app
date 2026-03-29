3a5bc63c3aec8cf73839965c9c1ba72c88257451
Teu Nome <teuemail@gmail.com>
2026-03-29 21:37:57 +0100

    mostrar token no admin

--- guests/admin.py
+++ guests/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from .models import Guest
 
-
 @admin.register(Guest)
 class GuestAdmin(admin.ModelAdmin):
-    list_display = ('full_name', 'event', 'phone', 'email', 'status')
+    list_display = ('full_name', 'event', 'phone', 'status')
+    readonly_fields = ('token',) 
     list_filter = ('event', 'status')
     search_fields = ('full_name', 'phone', 'email')
