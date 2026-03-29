d122f6d99672bcd5e6c949a929db03733d1dc4bf
Teu Nome <teuemail@gmail.com>
2026-03-29 23:51:40 +0100

    adiciona rota da pagina de convite

--- guests/urls.py
+++ guests/urls.py
@@ -4,6 +4,7 @@ from . import views
 urlpatterns = [
     path('confirm/<int:guest_id>/', views.confirm_guest, name='confirm_guest'),
     path('decline/<int:guest_id>/', views.decline_guest, name='decline_guest'),
+    path('invite/<uuid:token>/', views.invite_page, name='invite_page'),
     path('invite/<uuid:token>/<str:action>/', views.invite_response, name='invite_response'),
     path('guests/', views.guest_list, name='guest_list'),   
 ]
