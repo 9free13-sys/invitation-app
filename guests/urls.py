945cee24b73a2d0ee6361cc38d866d12b1a2505e
Teu Nome <teuemail@gmail.com>
2026-04-03 14:43:42 +0100

    m

--- guests/urls.py
+++ guests/urls.py
@@ -1,13 +1,14 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('', views.guest_list, name='guest_list'),
+
     path('confirm/<uuid:token>/', views.confirm_guest, name='confirm_guest'),
     path('decline/<uuid:token>/', views.decline_guest, name='decline_guest'),
+
+    path('invite/<uuid:token>/', views.invite_page, name='invite_page'),
     path('invite/<uuid:token>/<str:action>/', views.invite_response, name='invite_response'),
 
-    # eliminar convidado
     path('delete/<int:guest_id>/', views.delete_guest, name='delete_guest'),
 ]
--- guests/views.py
+++ guests/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Guest
-from events.models import Event
-from guests.models import Guest
 from django.contrib.auth.decorators import login_required
-from django.shortcuts import get_object_or_404, redirect
 from django.contrib import messages
 from .models import Guest
+from events.models import Event
+
 
 def create_invite(request):
     if request.method == 'POST':
@@ -24,28 +22,31 @@ def create_invite(request):
 
     return render(request, 'create_invite.html')
 
+
 @login_required
 def guest_list(request):
     guests = Guest.objects.filter(event__owner=request.user).order_by('-id')
     return render(request, 'guests/guest_list.html', {'guests': guests})
 
 
-def confirm_guest(request, guest_id):
-    guest = get_object_or_404(Guest, id=guest_id)
-    guest.confirmed = True
+def confirm_guest(request, token):
+    guest = get_object_or_404(Guest, token=token)
+    guest.status = 'confirmado'
     guest.save()
     return redirect('event_detail', event_id=guest.event.id)
 
 
-def decline_guest(request, guest_id):
-    guest = get_object_or_404(Guest, id=guest_id)
-    guest.confirmed = False
+def decline_guest(request, token):
+    guest = get_object_or_404(Guest, token=token)
+    guest.status = 'recusado'
     guest.save()
     return redirect('event_detail', event_id=guest.event.id)
+
 
 def invite_page(request, token):
     guest = get_object_or_404(Guest, token=token)
-    return render(request, 'guests/invite_page.html', {'guest': guest})
+    return render(request, 'guests/public_invite.html', {'guest': guest})
+
 
 def invite_response(request, token, action):
     guest = get_object_or_404(Guest, token=token)
